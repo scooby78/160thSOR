48aa3b0f62abc6d7157841b87931c9bcabbd10c6
scooby78 <scooby78@users.noreply.github.com>
2016-02-22 11:30:18 +1000

    V1.7 Various changes
    Added MEV m113
    Removed Morphine
    optimised repair capabilities

--- 160thSOR/cfgArmour.hpp
+++ 160thSOR/cfgArmour.hpp
@@ -2,11 +2,13 @@
 	Name: 160th SOR Mod (cfgArmour.hpp)
 	Author: Scooby
 	Date: 12/11/15 
-	Description: Only Armour units that are proven not "buggy" should be put in here.  Custom loadouts used.
+	Description:cfgArmour.hpp 
+	Note: Only Armour units that are proven not "buggy" should be put in here.
 */
 
 	class RHS_M2A3_BUSKIII;
 	class rhsusf_m113d_usarmy;
+	class rhsusf_m113d_usarmy_medical;
 	class rhsusf_m1a2sep1tuskid_usarmy;
 	class rhsusf_m1a2sep1tuskiid_usarmy;
 	class B_SDV_01_F;
@@ -18,121 +20,143 @@
 	class rhsusf_M1083A1P2_d_fmtv_usarmy;
 	
 //M2A3 (BUSK III)	
-	class SOR_M2A3_BUSKIII : RHS_M2A3_BUSKIII
+class SOR_M2A3_BUSKIII : RHS_M2A3_BUSKIII
 	{
 		faction = SOR_Faction_D;
 		vehicleclass = "SOR_Vehicles";
 		armor = 500; //was 325 protection against missiles, collisions and explosions
 		class TransportItems 
 		{
-			item_xx(ToolKit,1)
 			item_xx(ACE_FieldDressing,20);
-			item_xx(ACE_morphine,5);			
 		};
 		class TransportWeapons
 		{
-			weap_xx(M3CG,2)
+			weap_xx(rhs_weap_M136_hp,2)
 			weap_xx(rhs_weap_m4a1_carryhandle_grip,2)
 		};
 		class TransportMagazines
 		{
 			mag_xx(30Rnd_556x45_Stanag_Tracer_Red,20)
-			mag_xx(MAAWS_HEAT,10)
-			mag_xx(MAAWS_HEDP,10)
 			mag_xx(rhs_200rnd_556x45_M_SAW,4)
 			mag_xx(rhsusf_100Rnd_762x51,4)
 		};
-		class TransportBackpacks{};
+		class TransportBackpacks
+		{
+			pack_xx(SOR_Repair_Pack_D,1)			
+		};
 	};
 
-//M11A3 Armour 300 	
-	class SOR_rhsusf_m113d_usarmy : rhsusf_m113d_usarmy
+//M11A3 Armour	
+class SOR_rhsusf_m113d_usarmy : rhsusf_m113d_usarmy
 	{
 		faction = SOR_Faction_D;
 		vehicleclass = "SOR_Vehicles";
 		armor = 500; //was 200 protection against missiles, collisions and explosions
 		class TransportItems 
 		{
-			item_xx(ToolKit,1)
 			item_xx(ACE_FieldDressing,20);
-			item_xx(ACE_morphine,5);			
 		};
 		class TransportWeapons
 		{
-			weap_xx(M3CG,2)
+			weap_xx(rhs_weap_M136_hp,2)
 			weap_xx(rhs_weap_m4a1_carryhandle_grip,2)
 		};
 		class TransportMagazines
 		{
 			mag_xx(30Rnd_556x45_Stanag_Tracer_Red,20)
-			mag_xx(MAAWS_HEAT,10)
-			mag_xx(MAAWS_HEDP,10)
 			mag_xx(rhs_200rnd_556x45_M_SAW,4)
 			mag_xx(rhsusf_100Rnd_762x51,4)
 		};
-		class TransportBackpacks{};
+		class TransportBackpacks
+		{
+			pack_xx(SOR_Repair_Pack_D,1)			
+		};
 	};
+	
+//M11A3 Medical
+class SOR_rhsusf_m113d_usarmy_medical : rhsusf_m113d_usarmy_medical
+	{
+		faction = SOR_Faction_D;
+		vehicleclass = "SOR_Vehicles";
+		armor = 500; //was 200 protection against missiles, collisions and explosions
+		class TransportItems 
+		{
+			item_xx(ACE_Fielddressing,24)
+			item_xx(ACE_morphine,16)
+			item_xx(ACE_epinephrine,16)
+		};
+		class TransportWeapons
+		{
+			weap_xx(rhs_weap_M136_hp,2)
+			weap_xx(rhs_weap_m4a1_carryhandle_grip,2)
+		};
+		class TransportMagazines
+		{
+			mag_xx(30Rnd_556x45_Stanag_Tracer_Red,6)
+		};
+		class TransportBackpacks
+		{
+			pack_xx(SOR_Repair_Pack_D,1)			
+		};
+	};	
+	
 
 //Tusk I
-	class SOR_rhsusf_m1a2sep1tuskid_usarmy : rhsusf_m1a2sep1tuskid_usarmy
+class SOR_rhsusf_m1a2sep1tuskid_usarmy : rhsusf_m1a2sep1tuskid_usarmy
 	{
 		faction = SOR_Faction_D;
 		vehicleclass = "SOR_Vehicles";
 		armor = 700; //was 600 protection against missiles, collisions and explosions
 		class TransportItems 
 		{
-			item_xx(ToolKit,1)
 			item_xx(ACE_FieldDressing,20);
-			item_xx(ACE_morphine,5);
-			item_xx(ACE_Track,2)			
 		};
 		class TransportWeapons
 		{
-			weap_xx(M3CG,2)
+			weap_xx(rhs_weap_M136_hp,2)
 			weap_xx(rhs_weap_m4a1_carryhandle_grip,2)
 		};
 		class TransportMagazines
 		{
 			mag_xx(30Rnd_556x45_Stanag_Tracer_Red,20)
-			mag_xx(MAAWS_HEAT,10)
-			mag_xx(MAAWS_HEDP,10)
 			mag_xx(rhs_200rnd_556x45_M_SAW,4)
 			mag_xx(rhsusf_100Rnd_762x51,4)
 		};
-		class TransportBackpacks{};
+		class TransportBackpacks
+		{
+			pack_xx(SOR_Repair_Pack_D,1)			
+		};
 	};	
 
 //Tusk II
-	class SOR_rhsusf_m1a2sep1tuskiid_usarmy : rhsusf_m1a2sep1tuskiid_usarmy
+class SOR_rhsusf_m1a2sep1tuskiid_usarmy : rhsusf_m1a2sep1tuskiid_usarmy
 	{
 		faction = SOR_Faction_D;
 		vehicleclass = "SOR_Vehicles";
 		armor = 800; //was 600 protection against missiles, collisions and explosions
 		class TransportItems
 		{
-			item_xx(ToolKit,1)
 			item_xx(ACE_FieldDressing,20);
-			item_xx(ACE_morphine,5);
-			item_xx(ACE_Track,2)			
 		};
 		class TransportWeapons
 		{
-			weap_xx(M3CG,2)
+			weap_xx(rhs_weap_M136_hp,2)
 			weap_xx(rhs_weap_m4a1_carryhandle_grip,2)
 		};
 		class TransportMagazines
 		{
 			mag_xx(30Rnd_556x45_Stanag_Tracer_Red,20)
-			mag_xx(MAAWS_HEAT,10)
-			mag_xx(MAAWS_HEDP,10)
 			mag_xx(rhs_200rnd_556x45_M_SAW,4)
 			mag_xx(rhsusf_100Rnd_762x51,4)
 		};
-		class TransportBackpacks{};
+		class TransportBackpacks
+		{
+			pack_xx(SOR_Repair_Pack_D,1)			
+		};
 	};
 	
 //Sub	
-	class SOR_B_SDV_01_F : B_SDV_01_F
+class SOR_B_SDV_01_F : B_SDV_01_F
 	{
 		faction = SOR_Faction_D;
 		vehicleclass = "SOR_Vehicles";		
@@ -148,22 +172,21 @@
 	};	
 
 //Engineer transport
-	class SOR_rhsusf_m1025_d_s : rhsusf_m1025_d_s
+class SOR_rhsusf_m1025_d_s : rhsusf_m1025_d_s
 	{
 		faction = SOR_Faction_D;
 		vehicleclass = "SOR_Vehicles";
 		displayName = "M1025 (Engineer)";		
-		class TransportItems
-		{
-			item_xx(ToolKit,1)			
-		};
+		class TransportItems{};
 		class TransportWeapons
 		{
 			weap_xx(rhs_weap_m4a1_carryhandle_grip,4)
+			weap_xx(rhs_weap_M136_hp,2)
 		};
 		class TransportMagazines{};
 		class TransportBackpacks
 		{
+			pack_xx(SOR_Repair_Pack_D,1)
 			pack_xx(SOR_CombatPack_D,4)
 			pack_xx(SOR_OrdanancePack_D,1)	
 			pack_xx(B_Mortar_01_weapon_F,1)	
@@ -172,7 +195,7 @@
 	};
 
 //Engineer transport truck	
-	class SOR_rhsusf_M1083A1P2_d_fmtv_usarmy : rhsusf_M1083A1P2_d_fmtv_usarmy
+class SOR_rhsusf_M1083A1P2_d_fmtv_usarmy : rhsusf_M1083A1P2_d_fmtv_usarmy
 	{
 		faction = SOR_Faction_D;
 		vehicleclass = "SOR_Vehicles";
@@ -187,7 +210,6 @@
 		};
 		class TransportMagazines
 		{
-			mag_xx(MAAWS_HEAT,4)
 			mag_xx(rhs_200rnd_556x45_M_SAW,2)
 			mag_xx(rhsusf_100Rnd_762x51,2)
 		};
@@ -200,7 +222,7 @@
 	};	
 	
 //Engineer armored transport
-	class SOR_B_APC_Tracked_01_rcws_F : B_APC_Tracked_01_rcws_F
+class SOR_B_APC_Tracked_01_rcws_F : B_APC_Tracked_01_rcws_F
 	{
 		faction = SOR_Faction_D;
 		vehicleclass = "SOR_Vehicles";
@@ -211,13 +233,9 @@
 		class TransportWeapons
 		{
 			weap_xx(rhs_weap_m4a1_carryhandle_grip,4)
-			weap_xx(M3CG,2)			
+			weap_xx(rhs_weap_M136_hp,2)			
 		};
-		class TransportMagazines
-		{
-			mag_xx(MAAWS_HEAT,2)
-			mag_xx(MAAWS_HEDP,2)			
-		};
+		class TransportMagazines{};
 		class TransportBackpacks
 		{
 			pack_xx(SOR_CombatPack_D,4)
@@ -226,7 +244,7 @@
 	};	
 	
 //Troop transport
-	class SOR_rhsusf_m1025_d : rhsusf_m1025_d
+class SOR_rhsusf_m1025_d : rhsusf_m1025_d
 	{
 		faction = SOR_Faction_D;
 		vehicleclass = "SOR_Vehicles";
@@ -238,7 +256,7 @@
 	};
 	
 //Troop armed transport GL
-	class SOR_rhsusf_m1025_d_Mk19 : rhsusf_m1025_d_Mk19
+class SOR_rhsusf_m1025_d_Mk19 : rhsusf_m1025_d_Mk19
 	{
 		faction = SOR_Faction_D;
 		vehicleclass = "SOR_Vehicles";
@@ -250,7 +268,7 @@
 	};
 	
 //Troop armed transport MG
-	class SOR_rhsusf_m1025_d_m2 : rhsusf_m1025_d_m2
+class SOR_rhsusf_m1025_d_m2 : rhsusf_m1025_d_m2
 	{
 		faction = SOR_Faction_D;
 		vehicleclass = "SOR_Vehicles";
