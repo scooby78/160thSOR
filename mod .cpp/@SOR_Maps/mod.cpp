890cc1be96fc6d9d20687829f6747246f5679e21
scooby78 <scooby78@users.noreply.github.com>
2017-05-20 00:35:37 +1000

    .cpp files

---
+++ mod .cpp/@SOR_Maps/mod.cpp
@@ -0,0 +1,8 @@
+name = "160th SOR Map Collection";
+picture = "160thmaps.paa";
+actionName = "Website";
+action = "http://160th.net";
+logo = "160thmaps.paa";
+tooltip = "Cups terrain Pack"; 
+overview = "This adds 160th map packs";
+author = "Cups";
