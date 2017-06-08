1b231307ca763f5f886f96fba54711380a27fc6e
scooby78 <scooby78@users.noreply.github.com>
2017-06-08 11:15:28 +1000

    reverted smoke ammo class
    linked to changes made to G_40mm_Smoke in cgfAmmo

--- 160thSOR/cfgMagazines.hpp
+++ 160thSOR/cfgMagazines.hpp
@@ -8,7 +8,6 @@
 class CfgMagazines
 {
 	class rhsusf_100Rnd_762x51_m62_tracer;
-	class 1Rnd_HE_Grenade_shell;
 	
 	class rhs_mag_M829;
 	class rhs_mag_M830;
@@ -18,43 +17,6 @@ class CfgMagazines
 	{
 		ammo = "B_338_Ball";		
 	};
-	class 1Rnd_Smoke_Grenade_shell: 1Rnd_HE_Grenade_shell
-	{
-		author = "Bohemia Interactive";
-		displayName = "Smoke Round (White)";
-		picture = "\A3\Weapons_f\Data\UI\gear_UGL_Smokeshell_white_CA.paa";
-		ammo = "rhs_40mm_smoke_white";
-		descriptionShort = "Type: Smoke Round - White<br />Rounds: 1<br />Used in: EGLM, 3GL";
-		displayNameShort = "White Smoke";
-	};
-	class 1Rnd_SmokeRed_Grenade_shell: 1Rnd_Smoke_Grenade_shell
-	{
-		author = "Bohemia Interactive";
-		displayName = "Smoke Round (Red)";
-		picture = "\A3\Weapons_f\Data\UI\gear_UGL_Smokeshell_red_CA.paa";
-		ammo = "rhs_40mm_smoke_red";
-		displayNameShort = "Red Smoke";
-		descriptionShort = "Type: Smoke Round - Red<br />Rounds: 1<br />Used in: EGLM, 3GL";
-	};
-	class 1Rnd_SmokeGreen_Grenade_shell: 1Rnd_Smoke_Grenade_shell
-	{
-		author = "Bohemia Interactive";
-		displayName = "Smoke Round (Green)";
-		picture = "\A3\Weapons_f\Data\UI\gear_UGL_Smokeshell_green_CA.paa";
-		ammo = "rhs_40mm_smoke_green";
-		displayNameShort = "Green Smoke";
-		descriptionShort = "Type: Smoke Round - Green<br />Rounds: 1<br />Used in: EGLM, 3GL";
-	};	
-	class 1Rnd_SmokeYellow_Grenade_shell: 1Rnd_Smoke_Grenade_shell
-	{
-		author = "Bohemia Interactive";
-		displayName = "Smoke Round (Yellow)";
-		picture = "\A3\Weapons_f\Data\UI\gear_UGL_Smokeshell_yellow_CA.paa";
-		ammo = "rhs_40mm_smoke_yellow";
-		displayNameShort = "Yellow Smoke";
-		descriptionShort = "Type: Smoke Round - Yellow<br />Rounds: 1<br />Used in: EGLM, 3GL";
-	};	
-
 // RHS tank rounds
 	class rhs_mag_M829A3: rhs_mag_M829
 	{
