4ab3905cc8400ee37d72776af08aadb219f727bb
scooby78 <scooby78@users.noreply.github.com>
2017-06-10 16:22:50 +1000

    version changes

--- mod .cpp/@SOR_Content/mod.cpp
+++ mod .cpp/@SOR_Content/mod.cpp
@@ -3,6 +3,6 @@ picture = "160thcontent.paa";
 actionName = "Website";
 action = "http://160th.net";
 logo = "160thcontent.paa";
-tooltip = "ShackTac User Interface v1.2.0 / TAW View distance v1.5 / AresModAchillesExpansion  v007c / 3den Enhanced v1.8 / Knock on Tanks v1.1 / AdvancedSlingLoading v2.1.1 / AdvancedRappelling v1.3 / Advanced Towing v1.5 / Weapon-selector v1.2 / SMA v2.7.1"; 
+tooltip = "ShackTac User Interface v1.2.0 / TAW View distance v1.5 / AresModAchillesExpansion  v007d / 3den Enhanced v1.8 / Knock on Tanks v1.1 / AdvancedSlingLoading v2.1.1 / AdvancedRappelling v1.3 / Advanced Towing v1.5 / Weapon-selector v1.2 / SMA v2.7.1"; 
 overview = "This adds 160th additional content and frameworks";
 author = "Various";
--- mod .cpp/@SOR_Maps/mod.cpp
+++ mod .cpp/@SOR_Maps/mod.cpp
@@ -3,6 +3,6 @@ picture = "160thmaps.paa";
 actionName = "Website";
 action = "http://160th.net";
 logo = "160thmaps.paa";
-tooltip = "Cups terrain Pack"; 
+tooltip = "Cups terrain Pack v1.2.0 / Fallujah v1.2"; 
 overview = "This adds 160th map packs";
 author = "Cups";
