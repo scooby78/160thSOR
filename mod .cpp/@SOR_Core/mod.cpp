5ecfb50362dbff7de2b0bbdf1eab18ad82684dc5
scooby78 <mschuba@internode.on.net>
2017-09-03 16:31:06 +1000

    version change

--- mod .cpp/@SOR_Core/mod.cpp
+++ mod .cpp/@SOR_Core/mod.cpp
@@ -3,6 +3,6 @@ picture = "160thcore.paa";
 actionName = "Website";
 action = "http://160th.net";
 logo = "160thcore.paa";
-tooltip = "ACE3 v3.10.2 / ACEX 3.2.0 / RHSAFRF v0.4.2.2 / RHSUSAF v0.4.2.2 / RHSGREF v0.4.2.2 / CBA v3.4.0.170627 / TFAR v0.9.12 / Enhanced Movement 0.8.4.3 / 160th Loadouts v2.4.5 / 160th Opfor v2.1.1 / 160th Data v1.0.1"; 
+tooltip = "ACE3 v3.10.2 / ACEX 3.2.0 / RHSAFRF v0.4.2.2 / RHSUSAF v0.4.2.2 / RHSGREF v0.4.2.2 / CBA v3.4.0.170627 / TFAR v0.9.12 / Enhanced Movement 0.8.4.3 / 160th Loadouts v2.4.6 / 160th Opfor v2.1.1 / 160th Data v1.0.1"; 
 overview = "This adds 160th SOR core mod collections.";
 author = "Various";
