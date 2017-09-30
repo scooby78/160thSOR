e8e55f311855fc8712332985fa374b7e9b0ab260
scooby78 <mschuba@internode.on.net>
2017-09-30 11:28:13 +1000

    version changes

--- mod .cpp/@SOR_Content/mod.cpp
+++ mod .cpp/@SOR_Content/mod.cpp
@@ -3,6 +3,6 @@ picture = "160thcontent.paa";
 actionName = "Website";
 action = "http://160th.net";
 logo = "160thcontent.paa";
-tooltip = "ShackTac User Interface v1.2.0 / TAW View distance v1.5 / AresModAchillesExpansion  v008 / 3den Enhanced v1.8 / Knock on Tanks v1.1 / AdvancedSlingLoading v2.1.1 / AdvancedRappelling v1.3 / Advanced Towing v1.5 / 160th Weapon-selector v1.3 / SMA v2.7.1"; 
+tooltip = "ShackTac User Interface v1.2.0 / TAW View distance v1.5 / AresModAchillesExpansion v0.0.8c / 3den Enhanced v2.5 / Knock on Tanks v1.1 / AdvancedSlingLoading v2.1.1 / AdvancedRappelling v1.3 / Advanced Towing v1.5 / 160th Weapon-selector v1.4 / SMA v2.7.1"; 
 overview = "This adds 160th additional content and frameworks";
 author = "Various";
