5dd9c6d8d8b92803f2ad0f4e94f7b0cb67865eb4
scooby78 <mschuba@internode.on.net>
2017-10-14 18:40:15 +1000

    version change

--- mod .cpp/@SOR_Content/mod.cpp
+++ mod .cpp/@SOR_Content/mod.cpp
@@ -3,6 +3,6 @@ picture = "160thcontent.paa";
 actionName = "Website";
 action = "http://160th.net";
 logo = "160thcontent.paa";
-tooltip = "ShackTac User Interface v1.2.0 / TAW View distance v1.5 / AresModAchillesExpansion v0.0.9c / 3den Enhanced v2.5 / Knock on Tanks v1.1 / AdvancedSlingLoading v2.1.1 / AdvancedRappelling v1.3 / Advanced Towing v1.5 / 160th Weapon-selector v1.4 / SMA v2.7.1"; 
+tooltip = "ShackTac User Interface v1.2.0 / TAW View distance v1.5 / AresModAchillesExpansion v0.0.9c / 3den Enhanced v2.5 / Knock on Tanks v1.1 / AdvancedSlingLoading v2.1.1 / AdvancedRappelling v1.3 / Advanced Towing v1.5 / 160th Weapon-selector v1.4 / 160th Building Populator v0.3-beta / SMA v2.7.1"; 
 overview = "This adds 160th additional content and frameworks";
 author = "Various";
--- mod .cpp/@SOR_Core/mod.cpp
+++ mod .cpp/@SOR_Core/mod.cpp
@@ -3,6 +3,6 @@ picture = "160thcore.paa";
 actionName = "Website";
 action = "http://160th.net";
 logo = "160thcore.paa";
-tooltip = "ACE3 v3.10.2 / ACEX 3.2.0 / RHSAFRF v0.4.3 / RHSUSAF v0.4.3 / RHSGREF v0.4.3 / CBA v3.4.1.170912 / TFAR v0.9.12 / Enhanced Movement 0.8.4.3 / 160th Loadouts v2.4.9 / 160th Opfor v2.1.2 / 160th Data v1.0.2"; 
+tooltip = "ACE3 v3.10.2 / ACEX 3.2.0 / RHSAFRF v0.4.3 / RHSUSAF v0.4.3 / RHSGREF v0.4.3 / CBA v3.4.1.170912 / TFAR v0.9.12 / Enhanced Movement 0.8.4.3 / 160th Loadouts v2.4.9 / 160th Opfor v2.1.3 / 160th Data v1.0.2"; 
 overview = "This adds 160th SOR core mod collections.";
 author = "Various";
