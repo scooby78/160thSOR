bf479d56fc903fb8d31ed4a4f70926921d3bea02
destruktoid <destruktoid@gmail.com>
2017-10-21 00:18:42 +1100

    Create cfgFunctions.hpp

---
+++ 160thSOR_Vietnam/cfgFunctions.hpp
@@ -0,0 +1,9 @@
+class CfgFunctions {
+    class NAM {
+        class sound {
+            file = "160thSOR_Vietnam\functions";
+            class playSound;
+            class stopSound;
+        };
+    };
+};
