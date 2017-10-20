d989db0e895a19302cbb60fa93f4fc67927a6192
yettie79 <lenny.henderson@bigpond.com>
2017-10-20 22:46:34 +1100

    sound 50

--- 160thSOR_Vietnam/CfgSounds.hpp
+++ 160thSOR_Vietnam/CfgSounds.hpp
@@ -4,13 +4,13 @@ class CfgSounds {
 	sounds[] = {}; 
 	class Nam_ForWhatItsWorth { 
             name = "For what its worth"; 
-            sound[] = {"\160thSOR_Vietnam\sounds\For_What_Its_Worth.ogg", 300, 1};
+            sound[] = {"\160thSOR_Vietnam\sounds\For_What_Its_Worth.ogg", 50, 1};
             titles[] = {0,""}; 
 	}; 
 
 	class Nam_PaintItBlack { 
             name = "Paint it black"; 
-            sound[] = {"\160thSOR_Vietnam\sounds\Paint_it_Black.ogg", 300, 1};
+            sound[] = {"\160thSOR_Vietnam\sounds\Paint_it_Black.ogg", 50, 1};
             titles[] = {0,""}; 
 	}; 
 };
