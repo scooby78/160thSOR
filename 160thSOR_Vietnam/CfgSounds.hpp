c2619c4c4fb0197d09ab72b0f614bd62e4df130c
yettie79 <lenny.henderson@bigpond.com>
2017-10-20 23:29:47 +1100

    music

--- 160thSOR_Vietnam/CfgSounds.hpp
+++ 160thSOR_Vietnam/CfgSounds.hpp
@@ -4,13 +4,30 @@ class CfgSounds {
 	sounds[] = {}; 
 	class Nam_ForWhatItsWorth { 
             name = "For what its worth"; 
-            sound[] = {"\160thSOR_Vietnam\sounds\For_What_Its_Worth.ogg", 50, 1};
+            sound[] = {"\160thSOR_Vietnam\sounds\For_What_Its_Worth.ogg", 30, 1};
             titles[] = {0,""}; 
 	}; 
 
 	class Nam_PaintItBlack { 
             name = "Paint it black"; 
-            sound[] = {"\160thSOR_Vietnam\sounds\Paint_it_Black.ogg", 50, 1};
+            sound[] = {"\160thSOR_Vietnam\sounds\Paint_it_Black.ogg", 30, 1};
+            titles[] = {0,""}; 
+	}; 
+	
+	class Nam_houseoftherisingsun { 
+            name = "House of the rising sun"; 
+            sound[] = {"\160thSOR_Vietnam\sounds\houseoftherisingsun.ogg", 30, 1};
+            titles[] = {0,""}; 
+	}; 
+	
+	class Nam_nowheretorun { 
+            name = "No where to run"; 
+            sound[] = {"\160thSOR_Vietnam\sounds\nowheretorun.ogg", 30, 1};
+            titles[] = {0,""}; 
+	}; 
+		class Nam_runthroughthejungle { 
+            name = "run through the jungle"; 
+            sound[] = {"\160thSOR_Vietnam\sounds\runthroughthejungle.ogg", 30, 1};
             titles[] = {0,""}; 
 	}; 
 };
--- 160thSOR_Vietnam/cfgVehicles.hpp
+++ 160thSOR_Vietnam/cfgVehicles.hpp
@@ -10,7 +10,11 @@
 	class Radio_Old;
 	class Nam_Radio_Old: Radio_Old {
 	    displayName = "Vietnam Radio";
-	    init = "_radio = _this select 0;_radio addaction ["Paint it black",{(_this select 0) say3d ["Nam_PaintItBlack",30,1]}];_radio addaction ["For what its worth",{(_this select 0) say3d ["Nam_ForWhatItsWorth",30,1]}];";	
+	    init = "_radio = _this select 0;_radio addaction ["Paint it black",{(_this select 0) say3d ["Nam_PaintItBlack",30,1]}];
+			_radio addaction ["For what its worth",{(_this select 0) say3d ["Nam_ForWhatItsWorth",30,1]}]
+			_radio addaction ["House of the rising sun",{(_this select 0) say3d ["Nam_houseoftherisingsun",30,1]}]
+			_radio addaction ["No where to run",{(_this select 0) say3d ["Nam_nowheretorun",30,1]}]
+			_radio addaction ["Run through the jungle",{(_this select 0) say3d ["Nam_runthroughthejungle",30,1]}];";	
 	};
 	#include "cfgBackpacks.hpp"
 	#include "cfgTroopsNam.hpp"	
