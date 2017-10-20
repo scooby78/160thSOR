31df7515968260feaf3795e16f12244cfe9bc1b2
destruktoid <destruktoid@gmail.com>
2017-10-20 22:00:16 +1100

    Update CfgSounds.hpp

--- 160thSOR_Vietnam/CfgSounds.hpp
+++ 160thSOR_Vietnam/CfgSounds.hpp
@@ -1,16 +1,16 @@
 
 
+class CfgSounds {
 	sounds[] = {}; 
-		class music1 
-		{ 
-		name = "For what is worth"; 
-		sound[] = {"\160thSOR_Vietnam\sounds\For_What_Its_Worth.ogg", 300, 1};
-		titles[] = {0,""}; 
-		}; 
+	class Nam_ForWhatItsWorth { 
+            name = "For what its worth"; 
+            sound[] = {"\160thSOR_Vietnam\sounds\For_What_Its_Worth.ogg", 300, 1};
+            titles[] = {0,""}; 
+	}; 
 
-		class music2 
-		{ 
-		name = "Paint it black"; 
-		sound[] = {"\160thSOR_Vietnam\sounds\Paint_it_Black.ogg", 300, 1};
-		titles[] = {0,""}; 
-		}; 
+	class Nam_PaintItBlack { 
+            name = "Paint it black"; 
+            sound[] = {"\160thSOR_Vietnam\sounds\Paint_it_Black.ogg", 300, 1};
+            titles[] = {0,""}; 
+	}; 
+};
